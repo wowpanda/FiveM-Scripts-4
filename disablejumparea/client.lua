9ce7d23e87187d2469566cafb8e6175af88b792a
Hivanzz <49962220+Hivanzz@users.noreply.github.com>
2019-04-24 23:59:15 +0200

    Add files via upload

--- disablejumparea/client.lua
+++ disablejumparea/client.lua
@@ -1,7 +1,73 @@
 -- Coded by Hivan --
 --- For FiveM ---
 ---- Forum ----
---- @Hivan  --
+--- @Hivan - @Hivanzh --
+
+-- config --
+
+-- x, y, z, radius --
+
+disabled = {
+  {-29.596,-1103.832,26.422, 10}, -- Motorshop
+  {12.653217315674,-1105.1201171875,29.797031402588, 5} -- Armory
+}
+
+-- Script --
+
+Citizen.CreateThread(function()
+while true do
+  Citizen.Wait(0)
+  for i = 1, #disabled do
+    disabled2 = disabled[i] 
+  if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), disabled2[1],disabled2[2],disabled2[3]) < disabled2[4] then -- If distance (player - coords) < of radius then
+  	DisableControlAction(0,22,true) -- Space for Jump
+  x = disabled2[1]
+  y = disabled2[2]
+  z = disabled2[3]
+  r = disabled2[4]
+  end
+  end
+end
+end)
+
+Citizen.CreateThread(function()
+while true do
+  Citizen.Wait(100) 
+  if x then
+  for i = 1, #disabled do
+    disabled2 = disabled[i] 
+  if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), x,y,z) < r then -- If distance (player - coords) < of radius then
+  	if not notified then
+   SendNotification("~r~Jump disabled")
+   notified = true
+  end
+elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), x,y,z) > r then
+	if notified then
+	SendNotification("~g~Jump enabled")
+	notified = nil
+  x = 0
+  y = 0
+  z = 0
+  r = 0
+end
+end
+end
+end
+end
+end)
+
+-- do not touch --
+
+-- Coords --
+
+x = nil
+y = nil
+z = nil
+r = nil 
+
+-- fct --
+
+notified = nil
 
 -- General --
 
@@ -10,42 +76,3 @@ function SendNotification(msg)
   AddTextComponentString(msg)
   DrawNotification(true, false)
 end
-
--- config --
-
-x = 0
-y = 0
-z = 0
-radius = 10
-
--- fct --
-
-notified = nil
-
--- Script --
-
-Citizen.CreateThread(function()
-while true do
-  Citizen.Wait(0) 
-  if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), x,y,z) < radius then -- If distance (player - coords) < of radius then
-  	DisableControlAction(0,22,true) -- Space for Jump
-  end
-end
-end)
-
-Citizen.CreateThread(function()
-while true do
-  Citizen.Wait(0) 
-  if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), x,y,z) < radius then -- If distance (player - coords) < of radius then
-  	if not notified then
-   SendNotification("~r~Jump disabled")
-   notified = true
-  end
-elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), x,y,z) > radius then
-	if notified then
-	SendNotification("~g~Jump enabled")
-	notified = nil
-end
-end
-end
-end)
